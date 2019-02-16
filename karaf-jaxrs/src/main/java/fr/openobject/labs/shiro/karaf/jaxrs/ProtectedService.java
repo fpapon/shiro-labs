e3418381a103790d5fa519a4ded90f6dd4e59566
Francois Papon <fpapon@apache.org>
2019-02-16 18:28:22 +0400

    Fix tomcat

--- karaf-jaxrs/src/main/java/fr/openobject/labs/shiro/karaf/jaxrs/ProtectedService.java
+++ karaf-jaxrs/src/main/java/fr/openobject/labs/shiro/karaf/jaxrs/ProtectedService.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package fr.openobject.labs.shiro.tomcat.jaxrs;
+package fr.openobject.labs.shiro.karaf.jaxrs;
 
 import java.util.HashMap;
 import java.util.Map;
